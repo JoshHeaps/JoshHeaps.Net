f296ef8ae13ac427ebc473a5bedf9c38ccd335e9
jheaps <jheaps@conservice.com>
2024-11-22 15:26:47 -0700

    fix welcome message styling

--- JoshHeaps.Net/wwwroot/js/site.js
+++ JoshHeaps.Net/wwwroot/js/site.js
@@ -1,3 +1,7 @@
 ﻿window.onload = function () {
+    const element = document.getElementById("WelcomeMessage");
+    element.style.display = "none"; // Trigger reflow
+    element.offsetHeight; // Force reflow
+    element.style.display = ""; // Restore the original display
     typeText("#WelcomeMessage", "Hey! You found me! Since I have your attention, why don't I tell you about myself?");
 }
