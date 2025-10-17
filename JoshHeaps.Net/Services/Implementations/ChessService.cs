fd68ada82ec1225b5cfd51262e264885ce6bb5e7
jheaps <jheaps@conservice.com>
2025-10-17 12:14:37 -0600

    Get fancy wit it

--- JoshHeaps.Net/Services/Implementations/ChessService.cs
+++ JoshHeaps.Net/Services/Implementations/ChessService.cs
@@ -548,11 +548,7 @@ public class ChessService : IChessService
         => GenerateSlidingMoves(gs, piece, [(1, 1), (1, -1), (-1, 1), (-1, -1)]);
 
     private static List<Position> GenerateQueenMoves(GameState gs, ChessPiece piece)
-        => GenerateSlidingMoves(gs, piece,
-        [
-            (1, 0), (-1, 0), (0, 1), (0, -1),
-            (1, 1), (1, -1), (-1, 1), (-1, -1)
-        ]);
+        => [..GenerateRookMoves(gs, piece), ..GenerateBishopMoves(gs, piece)];
 
     private static List<Position> GenerateSlidingMoves(GameState gs, ChessPiece piece, (int dr, int dc)[] directions)
     {
